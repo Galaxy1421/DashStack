bf489b4166820a2c3c2a52ea37bde6e332eefbaf
Ghaida Alessa <gessa.1421@gmail.com>
2025-09-21 13:11:38 +0300

    feat(routing): add application routes (dashboard, team, orders, etc.)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard';
+import { OrderListsComponent } from './pages/orderLists/orderLists';
+import { TeamComponent } from './pages/team/team';
+import {AddMemberComponent} from './pages/addMember/addMember';
+import { EditMemberComponent } from './pages/editMember/editMember';
+import { FavoritesComponent } from './pages/favorites/favorites'
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'orders', component: OrderListsComponent },
+  { path: 'team', component: TeamComponent },
+{ path: 'addMember', component: AddMemberComponent },
+  { path: 'editMember/:id', component: EditMemberComponent },
+  { path: 'favorites', component: FavoritesComponent }
+
+
+];
