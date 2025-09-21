706213e6d73bd1f2ffe40c9cc8ac570fb54b79a2
Ghaida Alessa <gessa.1421@gmail.com>
2025-09-21 13:12:22 +0300

    chore(config): configure application providers (router, http, PrimeNG, animations)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import aura from '@primeuix/themes/aura';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideAnimations(),
+    provideRouter(routes),
+    provideHttpClient(),
+     providePrimeNG({
+      theme: { 
+        preset: aura,
+         options: {
+          darkModeSelector: '', 
+        }}
+    })
   ]
 };
